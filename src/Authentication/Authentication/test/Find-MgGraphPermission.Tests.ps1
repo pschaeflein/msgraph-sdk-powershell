c76d49cb473fa9dde155ac19df311ec655aedbbc
Adam Edwards <adamed@microsoft.com>
2021-07-14 09:22:50 -0700

    Add pending tests cases for Find-MgGraphPermission

--- src/Authentication/Authentication/test/Find-MgGraphPermission.Tests.ps1
+++ src/Authentication/Authentication/test/Find-MgGraphPermission.Tests.ps1
@@ -1,30 +1,38 @@
 # ------------------------------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All Rights Reserved. Licensed under the MIT License. See License in the project root for license information.
 # ------------------------------------------------------------------------------
-Describe "the Find-MgGraphPermission Command" {
-
-    BeforeAll {
+Describe "The Find-MgGraphPermission Command" {
+    BeforeAll{
         . (join-path $PSScriptRoot  ..\custom\Find-MgGraphPermission.ps1)
     }
 
-    Context "when executing the command" {
-        BeforeAll {
-            Mock Invoke-MgGraphRequest {
-
-                [PSCustomObject] @{
-                    Value = [PSCustomObject] @{
-                        Oauth2PermissionScopes=@();
-                        AppRoles=@()
-                    }
-                }
-            }
-        }
-
+    Context "When executing the command with empty service principal results from MS Graph" {
         It 'Executes successfully with no parameters' {
             { Find-MgGraphPermission | Out-Null } | Should -Not -Throw
             Assert-MockCalled Invoke-MgGraphRequest
         }
-
     }
 
+    Context "When executing the command using a constrained set of permissions returned by MS Graph" {
+        It 'Executes successfully with no parameters' -pending {
+        }
+
+        It "Retrieves the expected set of delegated and app-only permissions when a search string is specified" -pending {
+        }
+
+        It "Returns nothing and throws no exception if a search string is specified and there is no match" -pending {
+        }
+    }
+
+    Context "When executing the command without a connection to MS Graph" {
+        # Repeat cases abvoe
+    }
+
+    Context "When executing the command after initially not having a connection to MS Graph but getting it later" {
+        # Repeat cases abvoe
+    }
+
+    Context "When executing the command when initially not having access to read the service prinicpal but later having access" {
+        # Repeat cases abvoe
+    }
 }
