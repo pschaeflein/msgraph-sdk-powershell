f9cf9fbb68bf5c15ab800a50556bcd7f65bd6d89
Taofeek F. Obafemi-Babatunde <taofeekobafemibabatunde@gmail.com>
2021-07-22 12:46:32 -0400

    Proposed corrections from #771

--- src/Authentication/Authentication/test/Find-MgGraphPermission.Tests.ps1
+++ src/Authentication/Authentication/test/Find-MgGraphPermission.Tests.ps1
@@ -37,13 +37,20 @@ Describe "The Find-MgGraphPermission Command" {
         }
 
         It "Retrieves the expected set of delegated and app-only permissions when a search string is specified" {
-            { Find-MgGraphPermission mail | Out-Null } | Should -Not -Throw
+            { Find-MgGraphPermission 'ReadWrite' | Out-Null } | Should -Not -Throw
             Assert-MockCalled Invoke-MgGraphRequest
+            $test = Find-MgGraphPermission 'ReadWrite'
+            $test.GetType() | Should -Be 'System.Object[]'
+            $test.length | Should -Be 4
+            $test.PermissionType[0] | Should -Be 'admin'
+            $test.Id.GetType() | Should -Be 'System.Object[]'
         }
 
         It "Returns nothing and throws no exception if a search string is specified and there is no match" {
-            { Find-MgGraphPermission 'Microsoft is the greatest company' | Out-Null } | Should -Not -Throw
+            { Find-MgGraphPermission 'Nigeria has the best jollof' | Out-Null } | Should -Not -Throw
             Assert-MockCalled Invoke-MgGraphRequest
+            $test = Find-MgGraphPermission 'Nigeria has the best jollof'
+            $test | Should -Be $null
         }
     }
 
@@ -62,23 +69,32 @@ Describe "The Find-MgGraphPermission Command" {
         }
 
         It "Retrieves the expected set of delegated and app-only permissions when a search string is specified" {
-            { Find-MgGraphPermission mail | Out-Null } | Should -Not -Throw
+            { Find-MgGraphPermission 'ReadWrite' | Out-Null } | Should -Not -Throw
             Assert-MockCalled Invoke-MgGraphRequest
+            $test = Find-MgGraphPermission 'mail'
+            $test | Should -Not -Be $null
+            $test.length | Should -Be 17
+            $test.PermissionType[0] | Should -Be 'User'
+            $test.PermissionType[-1] | Should -Be 'Application'
+            $test.Id.GetType() | Should -Be 'System.Object[]'
         }
 
         It "Returns nothing and throws no exception if a search string is specified and there is no match" {
-            { Find-MgGraphPermission 'Microsoft is the greatest company' | Out-Null } | Should -Not -Throw
+            { Find-MgGraphPermission 'Nigeria has the best jollof' | Out-Null } | Should -Not -Throw
             Assert-MockCalled Invoke-MgGraphRequest
+            $test = Find-MgGraphPermission 'Nigeria has the best jollof'
+            $test | Should -Be $null
         }
     }
 
     Context "When executing the command after initially not having a connection to MS Graph but getting it later" {
-
         BeforeEach {
+            #$staticVariable = 'No Connection'
+
             Mock Invoke-MgGraphRequest {
 
                 Throw [System.Management.Automation.ValidationMetadataException]::new('mock connection error message')
-
+                
             }
         }
 
@@ -98,6 +114,9 @@ Describe "The Find-MgGraphPermission Command" {
         It "Retrieves the expected set of delegated and app-only permissions when a search string is specified" {
             { Find-MgGraphPermission mail | Out-Null } | Should -Not -Throw
             Assert-MockCalled Invoke-MgGraphRequest
+            $test = Find-MgGraphPermission 'ReadWrite'
+            $test | Should -Not -Be $null
+            $test.length | Should -Be 225
 
             Mock Invoke-MgGraphRequest{
                 $permissionData
@@ -105,23 +124,28 @@ Describe "The Find-MgGraphPermission Command" {
 
             { Find-MgGraphPermission mail | Out-Null } | Should -Not -Throw
             Assert-MockCalled Invoke-MgGraphRequest
+            $test = Find-MgGraphPermission 'ReadWrite'
+            $test | Should -Not -Be $null
+            $test.length | Should -Be 4
         }
 
         It "Returns nothing and throws no exception if a search string is specified and there is no match" {
-            { Find-MgGraphPermission 'Microsoft is the greatest company' | Out-Null } | Should -Not -Throw
+            { Find-MgGraphPermission 'Nigeria has the best jollof' | Out-Null } | Should -Not -Throw
             Assert-MockCalled Invoke-MgGraphRequest
+            $test = Find-MgGraphPermission 'Nigeria has the best jollof' | Should -Be $null
 
             Mock Invoke-MgGraphRequest{
                 $permissionData
             }
 
-            { Find-MgGraphPermission 'Microsoft is the greatest company' | Out-Null } | Should -Not -Throw
+            { Find-MgGraphPermission 'Nigeria has the best jollof' | Out-Null } | Should -Not -Throw
             Assert-MockCalled Invoke-MgGraphRequest
+            $test = Find-MgGraphPermission 'Nigeria has the best jollof'
+            $test | Should -Be $null
         }
     }
 
     Context "When executing the command when initially not having access to read the service prinicpal but later having access" {
-
         BeforeEach {
             Mock Invoke-MgGraphRequest {
 
@@ -153,18 +177,22 @@ Describe "The Find-MgGraphPermission Command" {
 
             { Find-MgGraphPermission mail | Out-Null } | Should -Not -Throw
             Assert-MockCalled Invoke-MgGraphRequest
+            $test = Find-MgGraphPermission 'ReadWrite'
+            $test.length | Should -Be 4
         }
 
         It "Returns nothing and throws no exception if a search string is specified and there is no match" {
-            { Find-MgGraphPermission 'Microsoft is the greatest company' | Out-Null } | Should -Not -Throw
+            { Find-MgGraphPermission 'Nigeria has the best jollof' | Out-Null } | Should -Not -Throw
             Assert-MockCalled Invoke-MgGraphRequest
 
             Mock Invoke-MgGraphRequest{
                 $permissionData
             }
 
-            { Find-MgGraphPermission 'Microsoft is the greatest company' | Out-Null } | Should -Not -Throw
+            { Find-MgGraphPermission 'Nigeria has the best jollof' | Out-Null } | Should -Not -Throw
             Assert-MockCalled Invoke-MgGraphRequest
+            $test = Find-MgGraphPermission 'Nigeria has the best jollof'
+            $test | Should -Be $null
         }
     }
 }
